00d252e00a06abe583070fcc15bb2edc6ec1216b
ibhf13 <ibhf13@gmail.com>
2023-07-09 21:14:05 +0200

    Update nils interface

--- src/utils/NilsInterfaces.ts
+++ src/utils/NilsInterfaces.ts
@@ -1,45 +1,31 @@
-interface Position {
-  startLine: number;
-  startColumn: number;
-  endLine: number;
-  endColumn: number;
+interface Report {
+  report_version: string;
+  report_timestamp: string;
+  target_language: string;
+  report_summary: ReportSummary;
+  project_info: ProjectInfo;
+  detector: Detector;
+  data_clumps: { [key: string]: DataClump };
 }
 
-interface Variable {
-  key: string;
+interface ReportSummary {
+  amount_data_clumps: number;
+}
+
+interface ProjectInfo {
+  project_name: string;
+  project_version: string;
+  project_commit: string;
+  additional: { [key: string]: any };
+}
+
+interface Detector {
   name: string;
-  type: string;
-  modifiers: string[];
-  position: Position;
-  to_variable?: Variable;
+  version: string;
+  options: Options;
 }
 
-interface DataClumpData {
-  [key: string]: Variable;
-}
-
-interface DataClump {
-  type: string;
-  key: string;
-  from_file_path: string;
-  from_class_or_interface_name: string;
-  from_class_or_interface_key: string;
-  from_method_name: string | null;
-  from_method_key: string | null;
-  to_file_path: string;
-  to_class_or_interface_name: string;
-  to_class_or_interface_key: string;
-  to_method_name: string | null;
-  to_method_key: string | null;
-  data_clump_type: string;
-  data_clump_data: DataClumpData;
-}
-
-interface DataClumps {
-  [key: string]: DataClump;
-}
-
-interface DetectorOptions {
+interface Options {
   sharedFieldParametersMinimum: number;
   sharedFieldParametersCheckIfAreSubtypes: boolean;
   subclassInheritsAllMembersFromSuperclass: boolean;
@@ -48,29 +34,43 @@ interface DetectorOptions {
   analyseMethodsWithUnknownHierarchy: boolean;
 }
 
-interface Detector {
+interface DataClump {
+  type: string;
+  key: string;
+  from_file_path: string;
+  from_class_or_interface_name: string;
+  from_class_or_interface_key: string;
+  from_method_name: string;
+  from_method_key: string;
+  to_file_path: string;
+  to_class_or_interface_name: string;
+  to_class_or_interface_key: string;
+  to_method_name: string;
+  to_method_key: string;
+  data_clump_type: string;
+  data_clump_data: { [key: string]: DataClumpData };
+}
+
+interface DataClumpData {
+  key: string;
   name: string;
-  version: string;
-  options: DetectorOptions;
+  type: string;
+  modifiers: any[];
+  to_variable: ToVariable;
+  position: Position;
 }
 
-interface ProjectInfo {
-  project_name: string;
-  project_version: string;
-  project_commit: string;
-  additional: Record<string, unknown>;
+interface ToVariable {
+  key: string;
+  name: string;
+  type: string;
+  modifiers: any[];
+  position: Position;
 }
 
-interface ReportSummary {
-  amount_data_clumps: number;
-}
-
-interface RootObject {
-  report_version: string;
-  report_timestamp: string;
-  target_language: string;
-  report_summary: ReportSummary;
-  project_info: ProjectInfo;
-  detector: Detector;
-  data_clumps: DataClumps;
+interface Position {
+  startLine: number;
+  startColumn: number;
+  endLine: number;
+  endColumn: number;
 }
