02a18c5bc34cc598b2b0596bac8e60dc2894b5af
ibhf13 <ibhf13@gmail.com>
2023-07-11 17:09:12 +0200

    updated test

--- src/test/FieldTests/simple/SimpleFieldsNegative.ts
+++ src/test/FieldTests/simple/SimpleFieldsNegative.ts
@@ -1,28 +1,4 @@
-class fieldsStringFieldsY {
-  private fieldsString: string;
-  private fieldsY: number;
-  // add default value if not exist || null
-  constructor(fieldsString: string = null, fieldsY: number = null) {
-    this.fieldsString = fieldsString;
-    this.fieldsY = fieldsY;
-  }
-
-  getFieldsString(): string {
-    return this.fieldsString;
-  }
-
-  setFieldsString(value: string): void {
-    this.fieldsString = value;
-  }
-
-  getFieldsY(): number {
-    return this.fieldsY;
-  }
-
-  setFieldsY(value: number): void {
-    this.fieldsY = value;
-  }
-}
+import { fieldsStringFieldsY } from "./SimpleFieldsNegativeNewClassCreated";
 
 class Fields22 {
   fieldsStringFieldsYInstance: fieldsStringFieldsY = new fieldsStringFieldsY(
---
+++ src/test/FieldTests/simple/SimpleFieldsNegativeNewClassCreated.ts
@@ -0,0 +1,25 @@
+export class fieldsStringFieldsY {
+  private fieldsString: string;
+  private fieldsY: number;
+  // add default value if not exist || null
+  constructor(fieldsString: string = null, fieldsY: number = null) {
+    this.fieldsString = fieldsString;
+    this.fieldsY = fieldsY;
+  }
+
+  getFieldsString(): string {
+    return this.fieldsString;
+  }
+
+  setFieldsString(value: string): void {
+    this.fieldsString = value;
+  }
+
+  getFieldsY(): number {
+    return this.fieldsY;
+  }
+
+  setFieldsY(value: number): void {
+    this.fieldsY = value;
+  }
+}
