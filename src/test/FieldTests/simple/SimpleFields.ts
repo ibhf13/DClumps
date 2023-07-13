9071ea1c31c9d80488fc6d04c23571f340648b5c
ibhf13 <ibhf13@gmail.com>
2023-07-13 19:49:57 +0200

    update test

--- src/test/FieldTests/simple/SimpleFields.ts
+++ src/test/FieldTests/simple/SimpleFields.ts
@@ -9,7 +9,9 @@ class Fields2 {
     this.fieldsY = 50;
     this.fieldsString = this.fieldsY.toString();
     let fields3Instance = new Fields3();
-    fields3Instance.fieldsY = 9;
+    if (fields3Instance.fieldsY === fields3Instance.fieldsY + 9);
+    let testObj = (fields3Instance.fieldsString =
+      fields3Instance.fieldsY.toString() + "hi");
     let obj = new Fields22(new FieldsStringFieldsY("name", undefined), 10);
     obj.fieldsStringFieldsYInstance.getFieldsY;
   }
@@ -23,7 +25,8 @@ class Fields3 {
   normalMethod(): void {
     let temp = this.fieldsY + 20;
     this.fieldsY = 10 * this.fieldsY;
-    this.fieldsString = "new string";
+    if (this.fieldsY === (this.fieldsY = this.fieldsY + 1) + 8)
+      this.fieldsString = "new string";
     console.log("fieldsString", this.fieldsString);
   }
 }
